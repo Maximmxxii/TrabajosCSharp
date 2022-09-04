a50e57d844eb81f76b1dc9911b15f80af8af307b
DiegoFercho <111025731+DiegoReyesMMXXII@users.noreply.github.com>
2022-09-04 00:09:39 -0500

    Update Startup.cs

--- Aplicacion/Frontend/Startup.cs
+++ Aplicacion/Frontend/Startup.cs
@@ -28,7 +28,7 @@ namespace Frontend
             //Inyectar el Contexto, se hace una sola vez
             services.AddDbContext<Persistencia.AppContext>();
             /**
-            *!
+            *!Esto es un comentario de ejemplo luego de hacer Fork
             */
 
             //Inyectar una dependencia por cada interfaz y la clase que la implementa
